8fe58f256472bdbab58c2c8c249403a503e43ceb
nfraz007 <nfraz007@gmail.com>
2019-04-14 12:43:38 +0530

    added localstorage in code

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -4,7 +4,7 @@
       <div class="gap"></div>
       <div class="row">
         <div class="col-md-6"><p>Markdown</p></div>
-        <div class="col-md-6"><p>Preview</p></div>
+        <div class="col-md-6"><p>Preview<span class="float-right">Words : {{words}} | Characters : {{characters}}</span></p></div>
       </div>
       <div class="row">
         <div class="col-md-6">
@@ -24,7 +24,29 @@ export default {
   name: 'Markdown',
   data() {
     return {
-      markdown: "# h1 heading\n## h2 heading\n### h3 heading\n#### h4 heading\n##### h5 heading\n###### h6 heading\n **bold**\n *italic*\n* list 1\n* list 2\n> quatation\nthis is link [my portfolio](http://nfraz.co.nf)\n`this is single line code`\n```\nthis is multiline code\nline 2\n```\n"
+      words: 0,
+      characters: 0,
+      markdown: (localStorage.markdown) ? localStorage.markdown : "# h1 heading\n## h2 heading\n### h3 heading\n#### h4 heading\n##### h5 heading\n###### h6 heading\n **bold**\n *italic*\n* list 1\n* list 2\n> quatation\nthis is link [my portfolio](http://nfraz.co.nf)\nthis is image ![random_image](https://picsum.photos/500/200)\n`this is single line code`\n```\nthis is multiline code\nline 2\n```\n"
+    }
+  },
+  mounted() {
+    this.count_words()
+    this.count_characters()
+  },
+  methods: {
+    count_words() {
+      // console.log(this.markdown.trim());
+      this.words = (this.markdown.trim()) ? this.markdown.split(' ').length : 0
+    },
+    count_characters() {
+      this.characters = this.markdown.length;
+    }
+  },
+  watch: {
+    markdown(markdown) {
+      localStorage.markdown = markdown
+      this.count_words()
+      this.count_characters()
     }
   }
 }
